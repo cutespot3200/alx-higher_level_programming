fa65d2326affc5114495921482a5030399a61476
cutespot3200 <id_bailey32@yahoo.com>
2023-07-11 05:11:07 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,23 +4,27 @@
 /**
  * check_cycle - Checks if a singly-linked list contains a cycle.
  * @list: A singly-linked list.
+ *
  * Return: If there is no cycle - 0.
  *         If there is a cycle - 1.
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *turtle, *hare;
 
-	if (!list)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
-	while (slow && fast && fast->next)
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		if (turtle == hare)
 			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
 	}
 
 	return (0);
