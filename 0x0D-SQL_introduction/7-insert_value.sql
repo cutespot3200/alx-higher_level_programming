3c1200ac8d59735e0cf3573f49c5dc9f3d976bd7
cutespot3200 <id_bailey32@yahoo>
2023-09-19 13:50:58 -0700

    érror fixed

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- A script that insert a new row into the table first_table.
-INSERT INTO `first_table` (`id` `name`) VALUES (89, `Best School`); 
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School"); 
